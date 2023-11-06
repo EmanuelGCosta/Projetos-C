96deddf4d62052418e1f6fd7581b32df4736e510
Emanuel Gonzaga da Costa <99223358+EmanuelGCosta@users.noreply.github.com>
2023-11-06 18:25:55 -0300

    atualizando main.c

--- Atividade 3 - Semáforos/main.c
+++ Atividade 3 - Semáforos/main.c
@@ -3,8 +3,6 @@
 #include <pthread.h>
 #include <windows.h>
 
-#define SEMAFOROS 4 
-
 char condicao[2][8] = { "fechado", "aberto" };
 
 pthread_mutex_t mutex;
@@ -13,37 +11,33 @@ void *semaforo1();
 void *semaforo2();
 
 
-
 int main() {
 	pthread_t thread1, thread2;
-    pthread_mutex_init(&mutex, NULL);
+	pthread_mutex_init(&mutex, NULL);
     
-    while(1){
-	    pthread_create(&thread1, NULL, semaforo1, NULL);	
+	while(1){
+		pthread_create(&thread1, NULL, semaforo1, NULL);	
 		pthread_create(&thread2, NULL, semaforo2, NULL);
 	
 		pthread_join(thread1, NULL);
 		pthread_join(thread2, NULL);	
 	}
 
-
-
 	
-    return 0;
+    	return 0;
 }
 
 void *semaforo1(){
 
-    pthread_mutex_lock(&mutex);
+	pthread_mutex_lock(&mutex);
     
-	printf("\n\nSemaforo 1: %s\n", condicao[1]);
+	printf("Semaforo 1: %s\n", condicao[0]);
 	printf("Semaforo 2: %s\n", condicao[1]);
 	printf("Semaforo 3: %s\n", condicao[0]);
 	printf("Semaforo 4: %s\n\n\n", condicao[1]);
 	sleep(3);
 	
 	pthread_mutex_unlock(&mutex);
-	
 
 	
 	return NULL;
@@ -56,10 +50,11 @@ void *semaforo2(){
 	printf("Semaforo 1: %s\n", condicao[1]);
 	printf("Semaforo 2: %s\n", condicao[0]);
 	printf("Semaforo 3: %s\n", condicao[1]);
-	printf("Semaforo 4: %s\n", condicao[0]);
+	printf("Semaforo 4: %s\n\n\n", condicao[0]);
 	sleep(3);
 
 	pthread_mutex_unlock(&mutex);
+
 	
 	return NULL;
 }
